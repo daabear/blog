c98bcc92d5951da7d00beb67e3d9e5afab955784
daabear <leowm112@gmail.com>
2025-04-12 21:50:36 -0400

    add routes and templates

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use tower_http::services::ServeDir;
 async fn main() {
     // build our application with a route
     let app = Router::new()
-        .route("/", get(handler))
+        .route("/", get(render_base_page))
+        .route("/blog", get(render_blog_page))
+        .route("/credits", get(render_credits_page))
         .nest_service("/templates", ServeDir::new("templates"))
         .nest_service("/assets", ServeDir::new("assets"));
 
@@ -20,9 +22,24 @@ async fn main() {
 
 #[derive(Template)]
 #[template(path = "base.html")]
-struct BaseTemplate {}
+struct PageBaseTemplate;
 
-async fn handler() -> Html<String> {
-    let template = BaseTemplate {};
-    Html(template.render().unwrap())
+#[derive(Template)]
+#[template(path = "blog.html")]
+struct PageBlogTemplate;
+
+#[derive(Template)]
+#[template(path = "credits.html")]
+struct PageCreditsTemplate;
+
+async fn render_base_page() -> Html<String> {
+    Html(PageBaseTemplate.render().unwrap())
+}
+
+async fn render_blog_page() -> Html<String> {
+    Html(PageBlogTemplate.render().unwrap())
+}
+
+async fn render_credits_page() -> Html<String> {
+    Html(PageCreditsTemplate.render().unwrap())
 }
